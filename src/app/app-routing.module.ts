f79a57b738c74e0fd63afd48fe5662679360e498
koderoom <koderoom.dev@gmail.com>
2020-09-01 10:42:33 +0530

    route.added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '1', component: Page1Component },
+  { path: '2', component: Page2Component },
+  { path: '3', component: Page3Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
